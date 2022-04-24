32123b7bf8b10b99a742245a27eba14a46665f06
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2022-04-24 12:29:33 +0530

    Chart Section

---
+++ chart.js
@@ -0,0 +1,54 @@
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var earning = document.getElementById('earning').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['Done', 'Underprocess', 'Fail'],
+        datasets: [{
+            label:'Traffic Source',
+            data: [1255, 1977, 1077],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+            
+            ],
+          
+        }]
+    },
+    options: {
+        responsive:true,
+    }
+});
+
+
+
+var earning = new Chart(earning, {
+    type: 'bar',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Api', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+        datasets: [{
+            label: 'Complete Cases',
+            data: [1211, 1911, 311, 511, 211, 311, 432, 343, 443, 434, 1222, 1333,],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            
+        }]
+    },
+    options: {
+       responsive:true,
+    }
+});
